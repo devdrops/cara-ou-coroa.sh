31beb6a58b3a8227517b6ec734b49571aec24918
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2016-05-05 17:03:32 -0300

    Reducing code size
    Days ago I've seen a russian roulette game on terminal, which uses the same logic but with different scenarios (well, if you got bad luck, instead of a bullet you would got a `rm -rf /`).
    
    Just a small refactor, for fun!

--- cara-ou-coroa.sh
+++ cara-ou-coroa.sh
@@ -3,8 +3,4 @@
 # cara-ou-coroa.sh
 # Copyright (c) 2016 Matheus Alves
 
-if [[ $(($RANDOM % 2)) == "0" ]]; then
-    echo "Cara"
-else
-    echo "Coroa"
-fi
+[ $[ $RANDOM % 2 ] = 0 ] && echo "Cara" || echo "Coroa"
